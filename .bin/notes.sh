ddc45ec805cdf72876520d831cbb64c9bb69af8c
David Michael <david.a.michael@emcins.com>
2021-02-19 08:24:29 -0600

    Update notes script

--- .bin/notes.sh
+++ .bin/notes.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+if [ ! -f $NOTED_DIR ]; then
+	git clone git@github.com:1davidmichael/Notes.git $NOTED_DIR
+fi
+
 # An opinionated script on setting up daily notes to be stored in git
 cd $NOTE_DIR
 mkdir -p $(date +"%Y/%m")
 
 cd $(date +"%Y/%m")
 if [ ! -f "$(date +'%m-%d-%Y').md" ]; then
-	    echo "# Notes for $(date +'%m/%d/%Y')" > "$(date +'%m-%d-%Y').md"
+	echo "# Notes for $(date +'%m/%d/%Y')" > "$(date +'%m-%d-%Y').md"
 fi
 
 git add .
