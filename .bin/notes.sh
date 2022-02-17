2740a16cfa525bd5f0fc65292f3e064be1377eed
David Michael <1.david.michael@gmail.com>
2022-02-16 22:41:01 -0600

    Add notes.sh functionality for days in the future

--- .bin/notes.sh
+++ .bin/notes.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
+
+if [ $# -eq 0 ]; then
+	folder_dir=$(date +"%Y/%m")
+	date=$(date +'%m-%d-%Y')
+else
+	day=$1
+	folder_dir=$(date --date="$1 day" +"%Y/%m")
+	date=$(date --date="$1 day" +'%m-%d-%Y')
+fi
 
 if [ ! -f $NOTED_DIR ]; then
 	git clone git@github.com:1davidmichael/Notes.git $NOTED_DIR
@@ -6,11 +15,11 @@ fi
 
 # An opinionated script on setting up daily notes to be stored in git
 cd $NOTE_DIR
-mkdir -p $(date +"%Y/%m")
+mkdir -p "$folder_dir"
 
-cd $(date +"%Y/%m")
-if [ ! -f "$(date +'%m-%d-%Y').md" ]; then
-	echo "# Notes for $(date +'%m/%d/%Y')" > "$(date +'%m-%d-%Y').md"
+cd "$folder_dir"
+if [ ! -f "$date.md" ]; then
+	echo "# Notes for $date" > "$date.md"
 fi
 
 git add .
