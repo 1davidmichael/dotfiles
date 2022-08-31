8c446dd9b78aa04e38b6563b2e9a615c146ae32f
David Michael <1.david.michael@gmail.com>
2022-08-31 08:37:39 -0500

    Fix syntax and logic issues with note script

--- .bin/notes.sh
+++ .bin/notes.sh
@@ -3,28 +3,30 @@
 # An opinionated script on setting up daily notes to be stored in git
 
 # Required environment variables:
-# $NOTED_DIR
+# $NOTE_DIR
 # $NOTE_REPO
 
 # Use number parameter to pre-create notes for future days
 if [ $# -eq 0 ]; then
-  older_dir=$(date +"%Y/%m")
   date=$(date +'%m-%d-%Y')
   folder_dir=$(date +"%Y/%m")
 else
-  day=$1
   folder_dir=$(date --date="$1 day" +"%Y/%m")
   date=$(date --date="$1 day" +'%m-%d-%Y')
 fi
 
 # Clone $NOTE_DIR if it doesn't yet exist
-if [ ! -f $NOTED_DIR ]; then
-  git clone "$NOTE_REPO" "$NOTED_DIR"
+if [ ! -d "$NOTE_DIR" ]; then
+  git clone "$NOTE_REPO" "$NOTE_DIR"
 fi
 
-cd $NOTE_DIR
+cd "$NOTE_DIR"
+
+# If git repo is in a clean state, pull any changes
+# and create a new branch
 if [ -z "$(git status --porcelain)" ]; then
   git pull
+  git checkout -b "$date"
 fi
 
 mkdir -p "$folder_dir"
@@ -35,12 +37,14 @@ if [ ! -f "$date.md" ]; then
 fi
 
 git add .
+
+# Select editor based on what is available
 if command -v code &> /dev/null; then
-  code $NOTE_DIR/
+  code "$NOTE_DIR/"
 elif command -v nvim &> /dev/null; then
-  nvim $NOTE_DIR/
+  nvim "$NOTE_DIR/"
 else
-  vim $NOTE_DIR/
+  vim "$NOTE_DIR/"
 fi
 
 # vim: ft=bash sw=2 ts=2
